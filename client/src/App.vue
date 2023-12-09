c4ba938e632bd5b29a9a53fd69d3b6a88cadeed9
yetkin karasungur <yetkinkrsngr@gmail.com>
2023-12-09 22:45:22 +0300

    some config has been change

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,15 @@
 <template>
+  <router-view />
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
-  <router-view/>
 </template>
-
+<script>
+export default {
+  name: 'App',
+};
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
